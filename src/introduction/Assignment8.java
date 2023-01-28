26a9f2a93eee66c4e347d3532d40f093feaccc1d
geeta <geetu.chelliah@gmail.com>
2023-01-28 11:58:27 +0530

    modification

--- src/introduction/Assignment8.java
+++ src/introduction/Assignment8.java
@@ -25,7 +25,8 @@ public class Assignment8 {
 		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
 		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
 		driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
-		System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value")); 
+		System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
+		System.out.println("git switching develop branch");
 		
 	}
 
